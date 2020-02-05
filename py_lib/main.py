f21e50d6245f31c1c6415d6aa230020ec58fdad3
Bogdan Yarushevskyi <xeedjr@gmail.com>
2020-02-05 15:41:53 +0200

    update

--- py_lib/main.py
+++ py_lib/main.py
@@ -12,15 +12,42 @@ def strTobytes(hex_string):
 def sendt(data):
     ser.write(str.encode('\n' + bytesTostr(data)));
 
+def event(packet):
+    print('Event : ')
+    print(packet)
+
+def process_in():
+    # wait for start
+    while ser.read(size=1) != str.encode('\n'):
+        pass
+    # wait len
+    len = int(str(ser.read(size=2), 'ascii'), 16);
+    # read packet
+    packet = bytes.fromhex(str(ser.read(len * 2), 'ascii'));
+    if (packet[0] == 1):
+        return packet;
+    else:
+        event(packet)
+
 def test ():
     ser.write(str.encode('\n' + bytesTostr([1, 1])));
-    ser.read_until();
-    slen = ser.read(size=2);
-    lens = strTobytes(str(slen, 'ascii'))
-    data = ser.read(int.from_bytes(lens, "big") * 2);
-    print (data)
+    packet = process_in();
+    #ser.read_until();
+    #slen = ser.read(size=2);
+    #lens = strTobytes(str(slen, 'ascii'))
+    #data = ser.read(int.from_bytes(lens, "big") * 2);
+    print (packet)
     
 def send_pipe_data (pipe, data):
+    req = [0, 2]
+    req.append(pipe);
+    req += data;
+    req[0] = len(req)-1;
+    ser.write(str.encode('\n' + bytesTostr(req)));
+    packet = process_in();
+    print (packet)
+
+def read_pipe_data (pipe, data):
     req = [0, 2]
     req.append(pipe);
     req += data;
@@ -35,13 +62,12 @@ def send_pipe_data (pipe, data):
 
 def loop():
     while True :
-        data = ser.read()
+        process_in()
 
 ser = serial.Serial('COM10')  # open serial port
 print(ser.name)         # check which port was really used
 
 test()
 send_pipe_data(0, [1, 18]);
-#read_pipe_data(0, [1, 18]);
 
 loop()
