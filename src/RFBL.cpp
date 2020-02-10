548cd1a716d87d5e66a83373d7f4cf7ed095af3e
Bogdan Yarushevskyi <xeedjr@gmail.com>
2020-02-11 01:33:57 +0200

    add hadling cobs return 0 fix issue with len

--- src/RFBL.cpp
+++ src/RFBL.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <string>
+#include <string.h>
 #include "ch.h"
 #include "hal.h"
 #include "cmsis_os.h"
@@ -34,8 +35,19 @@ RFBL::RFBL(RF24* radio) {
 	this->radio->openReadingPipe(3, pipes[3]);
 	this->radio->startListening();
 
-	auto id1 = osThreadCreate (osThread (RFBL_thread), this);
+	mutexRf_id = osMutexCreate  (osMutex (MutexRF));
+	if (mutexRf_id == NULL)  {
+		while (1) {};
+	}
+
+	thread_id = osThreadCreate (osThread (RFBL_thread), this);
 	auto id2 = osThreadCreate (osThread (RFBL_thread_serial), this);
+
+	auto timer_id = osTimerCreate (osTimer(Timer1), osTimerPeriodic, this);
+	if (timer_id == NULL)  {
+		while (1) {};
+	}
+	osTimerStart(timer_id, 10);
 }
 
 RFBL::~RFBL() {
@@ -43,19 +55,115 @@ RFBL::~RFBL() {
 }
 
 
-uint8_t RFBL::Tostr(uint8_t* data, size_t len, char* str) {
-	uint8_t index = 1;
-	str[0] = 0x0A;
-	str++;
-	for (int i = 0; i < len; i++) {
-		char tmp[4];
-		sprintf(tmp, "%02X", data[i]);
-		str[0] = tmp[0];
-		str[1] = tmp[1];
-		str += 2;
-		index += 2;
+#define StartBlock()	(code_ptr = dst++, code = 1)
+#define FinishBlock()	(*code_ptr = code)
+
+size_t RFBL::StuffData(const uint8_t *ptr, size_t length, uint8_t *dst)
+{
+	const uint8_t *start = dst, *end = ptr + length;
+	uint8_t code, *code_ptr; /* Where to insert the leading count */
+
+	StartBlock();
+	while (ptr < end) {
+		if (code != 0xFF) {
+			uint8_t c = *ptr++;
+			if (c != 0) {
+				*dst++ = c;
+				code++;
+				continue;
+			}
+		}
+		FinishBlock();
+		StartBlock();
 	}
-	return index;
+	FinishBlock();
+	return dst - start;
+}
+
+/*
+ * UnStuffData decodes "length" bytes of data at
+ * the location pointed to by "ptr", writing the
+ * output to the location pointed to by "dst".
+ *
+ * Returns the length of the decoded data
+ * (which is guaranteed to be <= length).
+ */
+size_t RFBL::UnStuffData(const uint8_t *ptr, size_t length, uint8_t *dst)
+{
+	const uint8_t *start = dst, *end = ptr + length;
+	uint8_t code = 0xFF, copy = 0;
+
+	for (; ptr < end; copy--) {
+		if (copy != 0) {
+			*dst++ = *ptr++;
+		} else {
+			if (code != 0xFF)
+				*dst++ = 0;
+			copy = code = *ptr++;
+			if (code == 0)
+				break; /* Source length too long */
+		}
+	}
+	return dst - start;
+}
+
+void RFBL::send_frame(uint8_t* data, uint8_t len) {
+	buffer_index = StuffData(data, len, &buffer[1]);
+	buffer[0] = 0;
+	buffer_index++;
+	buffer[buffer_index++] = 0;
+	chnWrite(&SDU1, (const uint8_t*)buffer, buffer_index);
+}
+
+void RFBL::process_frame(void) {
+	/// parse array
+	switch(read_buffer[0]) {
+	case 1: // test
+		{
+			AnswMessage answ = {0};
+			answ.s.cmdid = read_buffer[0];
+			answ.s.type = 1;
+			send_frame((uint8_t*)&answ.s, sizeof(answ.s));
+		}
+		break;
+	case 2:  /// Write to node
+		{
+			osStatus status;
+			uint8_t pipe_id = read_buffer[1];
+			uint8_t data_len =  read_buffer_index - 2;
+			uint8_t* data_p = &read_buffer[2];
+
+			status  = osMutexWait(mutexRf_id, osWaitForever);
+			if (status != osOK)  {
+			  // handle failure code
+			}
+			// First, stop listening so we can talk
+			radio->stopListening();
+
+			// Open the correct pipe for writing
+			radio->openWritingPipe(pipes[pipe_id]);
+
+			// Send the final one back.
+			bool res = radio->write( data_p, data_len );
+
+			// Now, resume listening so we catch the next packets.
+			radio->startListening();
+		    status = osMutexRelease(mutexRf_id);
+		    if (status != osOK)  {
+		      // handle failure code
+		    }
+
+			/// form nswer
+			AnswMessage answ = {0};
+			answ.s.cmdid = read_buffer[0];
+			answ.s.type = 1;
+			answ.s.result = res;
+			send_frame((uint8_t*)&answ.s, sizeof(answ.s));
+		}
+		break;
+	case 3:
+		break;
+	}
 }
 
 void RFBL::thread_serial(void) {
@@ -67,79 +175,25 @@ void RFBL::thread_serial(void) {
 		/// wait 0xOA
 		len = chnRead(&SDU1, (uint8_t*)&ch, 1);
 		if (len != 1) {
+			osThreadYield();
+			buffer_index = 0;
 			continue;
 		}
-		if (ch != 0x0A) {
+		if (ch == 0x00) {
+			/// scip start frame chracter
+			if (buffer_index > 0) {
+				// we have data process it
+				read_buffer_index = UnStuffData(buffer, buffer_index, read_buffer);
+				if (read_buffer_index != 0) {
+					process_frame();
+				} else {
+					//// TODO some errror of decoding previous data len
+				};
+			};
+			buffer_index = 0;
 			continue;
-		}
-
-		/// wAIT LEN
-		len = chnRead(&SDU1, (uint8_t*)&read_buffer[0], 2);
-		if (len != 2) {
-			continue;
-		}
-		sscanf((char*)&read_buffer[0], "%02X", &len);
-		len *= 2;
-		if (len == 0 || len > 255) {
-			continue;
-		}
-		/// get data
-		auto res = chnRead(&SDU1, (uint8_t*)&read_buffer[2], len);
-		if (res != len) {
-			continue;
-		}
-
-		/// convert to bytes
-		for (int i = 0, bytes_index = 0; i < (len + 2); i+=2) {
-			int byte = 0;
-			sscanf((char*)&read_buffer[i], "%02X", &byte);
-			read_buffer[bytes_index++] = byte;
-		}
-		/// parse array
-		switch(read_buffer[1]) {
-		case 1: // test
-			{
-				char str[20] = {0};
-				AnswMessage answ = {0};
-				answ.s.cmdid = read_buffer[1];
-				answ.s.type = 1;
-				answ.s.len = sizeof (answ.s) - 1;
-				auto len = Tostr(answ.b, sizeof(answ.s), str);
-
-				chnWrite(&SDU1, (const uint8_t*)str, len);
-			}
-			break;
-		case 2:  /// Write to node
-			{
-				uint8_t pipe_id = read_buffer[2];
-				uint8_t data_len =  read_buffer[0] - 2;
-				uint8_t* data_p = &read_buffer[3];
-
-				// First, stop listening so we can talk
-				radio->stopListening();
-
-				// Open the correct pipe for writing
-				radio->openWritingPipe(pipes[pipe_id]);
-
-				// Send the final one back.
-				bool res = radio->write( data_p, data_len );
-
-				// Now, resume listening so we catch the next packets.
-				radio->startListening();
-
-				/// form nswer
-				char str[20] = {0};
-				AnswMessage answ = {0};
-				answ.s.cmdid = read_buffer[1];
-				answ.s.type = 1;
-				answ.s.result = res;
-				answ.s.len = sizeof (answ.s)-1;
-				auto len = Tostr(answ.b, sizeof (answ.s), str);
-				chnWrite(&SDU1, (const uint8_t*)str, len);
-			}
-			break;
-		case 3:
-			break;
+		} else {
+			buffer[buffer_index++] = ch;
 		}
 	}
 }
@@ -147,15 +201,40 @@ void RFBL::thread_serial(void) {
 
 void RFBL::thread(void) {
 	while(1) {
-		Message msg;
-		mb.pop(msg);
-		if ( radio->available() ) {
-			uint8_t data [25] = {0};
-	        // Fetch the payload, and see if this was the last one.
-	        auto done = radio->read( data, sizeof(data) );
-		//	chnWrite(&SDU1, (const uint8_t*)data, sizeof(data));
-//			chnWrite(&SDU1, (const uint8_t*)"\n\r", 2);
-		}
+		osStatus status;
+		osEvent ev;
+
+		ev = osSignalWait(0, osWaitForever);
+		if ((ev.value.signals & kTimer1) > 0) {
+			uint8_t pipe_num = 0;
+
+			status  = osMutexWait(mutexRf_id, osWaitForever);
+			if (status != osOK)  {
+			  // handle failure code
+			}
+			if ( radio->available(&pipe_num) ) {
+				uint8_t data [25] = {0};
+				// Fetch the payload, and see if this was the last one.
+				bool done = false;
+				while (!done)
+				{
+					uint8_t len = radio->getDynamicPayloadSize();
+					done = radio->read( data, len );
+
+					EventMessage evmsg;
+					evmsg.s.type = 2;
+					evmsg.s.pipe_num = pipe_num;
+					memcpy(evmsg.s.data, data, len);
+					send_frame((uint8_t*)&evmsg.s, (len + sizeof(evmsg.s.type) + sizeof(evmsg.s.pipe_num)));
+				}
+
+				osSignalSet(thread_id, kTimer1);
+			}
+			status = osMutexRelease(mutexRf_id);
+			if (status != osOK)  {
+			  // handle failure code
+			}
+		};
 	}
 }
 
--- src/RFBL.h
+++ src/RFBL.h
@@ -22,6 +22,7 @@ class RFBL {
 		p->thread();
 	};
 	osThreadDef (RFBL_thread, osPriorityNormal, 1024, 0);              // define Thread and specify to allow three instances
+	osThreadId thread_id;
 
 	void thread_serial(void);
 	static void RFBL_thread_serial(const void* thisp) {
@@ -30,9 +31,11 @@ class RFBL {
 	};
 	osThreadDef (RFBL_thread_serial, osPriorityNormal, 1024, 0);              // define Thread and specify to allow three instances
 
+	osMutexDef (MutexRF);
+	osMutexId mutexRf_id;
+
 	typedef union {
 		struct {
-			uint8_t len;
 			uint8_t type;
 			// - Common
 			uint8_t cmdid;
@@ -42,9 +45,9 @@ class RFBL {
 	} AnswMessage;
 	typedef union {
 		struct {
-			uint8_t len;
 			uint8_t type;
 			// - Common
+			uint8_t pipe_num;
 			uint8_t data[40];
 		}s;
 		uint8_t b[30];
@@ -62,12 +65,28 @@ class RFBL {
 	} Message;
 	MailBox<Message, 10> mb;
 
+	enum {
+		kTimer1 = 1 << 0,
+	};
+
+	void static BL_Timer1_Callback  (void const *arg) {
+		RFBL* p = (RFBL*)arg;
+		osSignalSet(p->thread_id, kTimer1);
+	};
+	osTimerDef (Timer1, BL_Timer1_Callback);
+
 	// Radio pipe addresses for the 2 nodes to communicate.
 	const uint64_t pipes[4] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL, 0xF0F0F0F0D3LL, 0xF0F0F0F0D4LL};
 
-	uint8_t read_buffer[256];
+	uint8_t buffer[256] = {0};
+	uint8_t buffer_index = 0;
+	uint8_t read_buffer[256] = {0};
+	uint8_t read_buffer_index = 0;
+	size_t StuffData(const uint8_t *ptr, size_t length, uint8_t *dst);
+	size_t UnStuffData(const uint8_t *ptr, size_t length, uint8_t *dst);
 
-	uint8_t Tostr(uint8_t* data, size_t len, char* str);
+	void send_frame(uint8_t* data, uint8_t len);
+	void process_frame(void);
 public:
 	RFBL(RF24* radio);
 	virtual ~RFBL();
